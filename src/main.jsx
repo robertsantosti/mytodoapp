a85836f1921b16b52d90aa09cd4cd508a8078c95
Robert Santos <robertsantosti@outlook.com>
2023-08-22 20:09:42 -0300

    Definindo as rotas do sistema

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { AppRoutes } from './routes/AppRoutes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+    <AppRoutes/>
   </React.StrictMode>,
 )
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { HomePage } from "../pages/home/HomePage"
+import { TodoPage } from "../pages/todo/TodoPage"
+import { ErrorPage } from "../pages/error/ErrorPage"
 
 export const AppRoutes = () => {
   return(
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route/>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/todo" element={<TodoPage/>}/>
+        <Route path="/todo/:id" element={<TodoPage/>}/>
+        <Route path="*" element={<ErrorPage/>}/>
       </Routes>
-    </BrowserRouter>
+    </Router>
   )
 }
