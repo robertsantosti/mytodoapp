f58b89f5fc4b9d6be9312b93013f52b8661b5748
Robert Santos <robertsantosti@outlook.com>
2023-08-22 20:16:18 -0300

    Definindo rotas que utilizaram o layout

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { HomePage } from "../pages/home/HomePage"
 import { TodoPage } from "../pages/todo/TodoPage"
 import { ErrorPage } from "../pages/error/ErrorPage"
+import { AppLayout } from "../layouts/AppLayout"
 
 export const AppRoutes = () => {
   return(
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/todo" element={<TodoPage/>}/>
-        <Route path="/todo/:id" element={<TodoPage/>}/>
+        <Route path="/" element={<AppLayout/>}>
+          <Route index element={<HomePage/>}/>
+          <Route path="/todo" element={<TodoPage/>}/>
+          <Route path="/todo/:id" element={<TodoPage/>}/>
+        </Route>
         <Route path="*" element={<ErrorPage/>}/>
       </Routes>
     </Router>
