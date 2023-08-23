419d3d5ee0c2812a3dc663da34d3cdccbdd1da10
Robert Santos <robertsantosti@outlook.com>
2023-08-23 19:19:54 -0300

    Criando estrutura da Pagina de Erro

--- src/pages/error/ErrorPage.jsx
+++ src/pages/error/ErrorPage.jsx
@@ -1,6 +1,20 @@
+import { useNavigate } from "react-router-dom"
+import { ButtonComponent } from "../../components/button/ButtonComponent"
 
 export const ErrorPage = () => {
+  const navigate = useNavigate();
+
+  const navigateTo = (route) => {
+    navigate(route);
+  }
+
   return (
-    <div>ErrorPage</div>
+    <div className="ErrorContainer">
+      <h3>Opss! Página não encontrada...</h3>
+      <p>Não Conseguimos encontrar a página solicitada!</p>
+      <ButtonComponent onClick={() => { navigateTo('/') }}>
+        Votar para pagina inicial
+      </ButtonComponent>
+    </div>
   )
 }
