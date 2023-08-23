da2cce0b22aaf2c97e4fa47a2f5c4864d1a79dd6
Robert Santos <robertsantosti@outlook.com>
2023-08-22 22:02:23 -0300

    Criando botão para criar uma tarefa

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,14 +1,21 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import { HeaderComponent } from "../components/header/HeaderComponent"
+import { ButtonComponent } from "../components/button/ButtonComponent"
 
 export const AppLayout = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('todo');
+  }
+
   return (
     <div className="Layout">
       <HeaderComponent/>
 
       <main className="Main">
         <div className="ActionsContainer">
-          <button>Ação</button>
+          <ButtonComponent onClick={handleClick}>Criar Tarefa</ButtonComponent>
         </div>
 
         <section className="MainContainer">
