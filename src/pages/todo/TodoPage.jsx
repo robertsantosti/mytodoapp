f87299f05d13a0033df77408cabfbb835586cb3f
Robert Santos <robertsantosti@outlook.com>
2023-08-23 19:49:02 -0300

    Logica para exibição do formulario na TodoPage

--- src/pages/todo/TodoPage.jsx
+++ src/pages/todo/TodoPage.jsx
@@ -1,9 +1,35 @@
+import { useParams } from 'react-router-dom'
 import { FormComponent } from '../../components/form/FormComponent'
+import { useEffect, useState } from 'react';
+
+const todos = [
+  {
+    id: 1,
+    title: 'Tarefa 1',
+    description: 'Lorem ipsum dolor sit',
+    status: false,
+  },
+  {
+    id: 2,
+    title: 'Tarefa 2',
+    description: 'Lorem ipsum dolor sit',
+    status: true,
+  },
+]
 
 export const TodoPage = () => {
+  const { id } = useParams();
+  const [todo, setTodo] = useState();
+
+  useEffect(() => {
+    if(id) {
+      setTodo(todos.find(item => item.id === Number(id)))
+    }
+  }, [])
+
   return (
     <div className="TodoFormContainer">
-      <FormComponent/>
+      <FormComponent todo={todo}/>
     </div>
   )
 }
