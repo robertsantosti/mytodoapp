baa1234d5c01e5af5858536307e0f283b23041e1
Robert Santos <robertsantosti@outlook.com>
2023-08-23 19:27:05 -0300

    Inserindo cards na pagina home

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,6 +1,28 @@
+import { CardComponent } from '../../components/card/CardComponent'
+
+const todos = [
+  {
+    id: 1,
+    title: 'Tarefa 1',
+    description: 'Lorem ipsum dolor sit',
+    status: false,
+  },
+  {
+    id: 2,
+    title: 'Tarefa 2',
+    description: 'Lorem ipsum dolor sit',
+    status: true,
+  },
+]
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="HomeContainer">
+      <div className="CardsContainer">
+        { todos.map(todo => 
+          <CardComponent todo={todo} key={todo.id}/> 
+        )}
+      </div>
+    </div>
   )
 }
