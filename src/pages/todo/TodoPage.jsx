20f5a6e2e0639be8057fc1ba434c9339d1102586
Robert Santos <robertsantosti@outlook.com>
2023-08-24 21:22:08 -0300

    Consumindo serviço na pagina de Todos

--- src/pages/todo/TodoPage.jsx
+++ src/pages/todo/TodoPage.jsx
@@ -1,17 +1,18 @@
 import { useParams } from 'react-router-dom'
 import { FormComponent } from '../../components/form/FormComponent'
 import { useEffect, useState } from 'react';
-import { MockUtils } from '../../utils/mock';
+import { ApiService } from '../../services/ApiService'
 
 import * as Styled from './TodoPage.style';
 
 export const TodoPage = () => {
   const { id } = useParams();
   const [todo, setTodo] = useState();
+  const service = new ApiService('tasks');
 
   useEffect(() => {
     if(id) {
-      setTodo(MockUtils.Todos.find(item => item.id === Number(id)))
+      service.Show(id).then(response => setTodo(response))
     }
   }, [])
 
