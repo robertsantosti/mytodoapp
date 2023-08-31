88922037beb63142bb69efd37f9e972ef4f5c288
Robert Santos <robertsantosti@outlook.com>
2023-08-31 20:17:16 -0300

    Testando componente de Botão (wip)

---
+++ src/components/button/ButtonComponent.test.jsx
@@ -0,0 +1,64 @@
+import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { ButtonComponent } from './ButtonComponent'
+import { vi } from 'vitest';
+
+const mockFn = vi.fn();
+
+describe('Test Suite ButtonComponent', () => {
+  beforeEach(() => {
+    render(<ButtonComponent>Meu Botão</ButtonComponent>)
+  })
+
+  //Deve renderizar o component
+  it('Should be able to render', () => {
+    const button = screen.getByTestId('button-component');
+    expect(button).toBeInTheDocument();
+  })
+
+  //ButtonComponent não deve estar desabilitado
+  it('Should be not disabled', () => {
+    const button = screen.getByTestId('button-component');
+    expect(button).not.toBeDisabled();
+  })
+
+  //ButtonComponent não deve exibir o texto "Carregando"
+  it('Should be not show text "Carregando"', () => {
+    const button = screen.getByTestId('button-component');
+    expect(button).not.toHaveTextContent('Carregando');
+  })
+
+  //ButtonComponent não deve exibir o texto "Carregando"
+  it('Should be type button', () => {
+    const button = screen.getByTestId('button-component');
+    expect(button).toHaveAttribute('type', 'button') 
+  })
+
+  it('Should be show text "Meu Botão"', () => {
+    const button = screen.getByTestId('button-component');
+    expect(button).toHaveTextContent('Meu Botão'); 
+  })
+})
+
+describe('Test Suite ButtonComponent disabled', () => {
+  it('Should be disabled', () => {
+    render(<ButtonComponent disabled={true}>Meu Botão</ButtonComponent>)
+    const button = screen.getByTestId('button-component');
+    expect(button).toBeDisabled();
+  })
+})
+
+describe('Test Suite ButtonComponent loading', () => {
+  it('Should be loading', () => {
+    render(<ButtonComponent loading={true}>Meu Botão</ButtonComponent>)
+    const button = screen.getByTestId('button-component');
+    expect(button).toHaveTextContent('Carregando');  
+  })
+})
+
+describe('Test Suite ButtonComponent has click action', () => {
+  it('should be click button call function', () => {
+    const button = render(<ButtonComponent onClick={() => {}}>Meu Botão</ButtonComponent>)
+    const buttonSpy = vi.spyOn(button, 'click');
+  })
+})
